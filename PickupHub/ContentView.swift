1b3bcf6c606ad91e08f8baf4180ead74bb5d626c
Adam <44130552+adsalad@users.noreply.github.com>
2022-05-17 22:11:51 -0400

    Initial Commit

---
+++ PickupHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PickupHub
+//
+//  Created by Adam S on 2022-05-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PickupHub/PickupHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickupHubApp.swift
+//  PickupHub
+//
+//  Created by Adam S on 2022-05-17.
+//
+
+import SwiftUI
+
+@main
+struct PickupHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
